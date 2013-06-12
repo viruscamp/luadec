4707dd88039a57839e762eecd4c8076a940b2455
viruscamp@gmail.com <viruscamp@users.noreply.github.com>
2013-06-12 11:49:21 +0000

    Makefile could generate 3 versions of luadec : release, debug and with memwatch.

--- lua-5.1.4/src/luadec/common.h
+++ lua-5.1.4/src/luadec/common.h
@@ -1,19 +1,18 @@
 #pragma once
+
 #ifndef LUADEC_COMMON_H
 #define LUADEC_COMMON_H
 
 #ifdef _DEBUG
 
-#ifdef MEMCHECK_VLD
-#include <wchar.h>
-#include <vld.h>
-#endif
-
-#ifdef MEMCHECK_MEMWATCH
-#define MEMWATCH
-#define MW_STDIO
-#include <string.h>
-#include "memwatch.h"
+#if defined MEMCHECK_VLD
+	#include <wchar.h>
+	#include <vld.h>
+#elif defined MEMCHECK_MEMWATCH
+	#define MEMWATCH
+	#define MW_STDIO
+	#include <string.h>
+	#include "memwatch.h"
 #endif
 
 #endif
