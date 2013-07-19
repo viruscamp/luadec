70738f69cdcec16507aedad8ceec930c9c36f5dd
viruscamp@gmail.com <viruscamp@gmail.com@36995d71-c58e-7c83-4a3e-2314ce43cc21>
2013-07-19 08:38:28 +0000

    change List to a doubly-linked list change F->vpend to use List instead of fixed-size VarStack

--- trunk/lua-5.1/src/luadec/decompile.h
+++ trunk/lua-5.1/src/luadec/decompile.h
@@ -7,30 +7,30 @@
 typedef struct BoolOp_ BoolOp;
 
 struct BoolOp_ {
-   //ListItem super;
-   char *op1;
-   char *op2;
-   OpCode op;
-   int neg;
-   int pc;
-   int dest;
+	//ListItem super;
+	char *op1;
+	char *op2;
+	OpCode op;
+	int neg;
+	int pc;
+	int dest;
 };
 
 typedef struct Endif_ Endif;
 
 struct Endif_ {
-   int addr;
-   Endif* next;
+	int addr;
+	Endif* next;
 };
 
 typedef struct Statement_ Statement;
 
 struct Statement_ {
-   ListItem super;
-   char *code;
-   int line;
-   int indent;
-   int backpatch;
+	ListItem super;
+	char *code;
+	int line;
+	int indent;
+	int backpatch;
 };
 
 typedef enum {
@@ -61,103 +61,103 @@ struct LoopItem_ {
 
 typedef struct Function_ Function;
 struct Function_ {
-   /* program counter during symbolic interpretation */
-   int pc;
-   /* These act as the VM registers */
-   char *R[MAXARG_A];
-   /* These store the priority for the operation that stored the value in each
-      register */
-   int Rprio[MAXARG_A];
-   /* Boolean values indicating if register holds a table */
-   int Rtabl[MAXARG_A];
-   /* Registers standing for local variables. */
-   int Rvar[MAXARG_A];
-   /* Registers used in call returns. */
-   int Rcall[MAXARG_A];
-   /* This is the Lua proto for the function */
-   const Proto *f;
-   /* This is a stack for creation of tables */
-   List tables;
-   /* 'a' of last CALL instruction -- used with 0-param CALLs */
-   int lastCall;
-   /* Pending code to be flushed */
-   /* FIXME: Needs a better data structure */
-   char* vpendVal[MAXARG_A];
-   int internal[MAXARG_A];
-   int Rpend[MAXARG_A];
-   /* State variables for the TEST instruction. */
-   int testpending;
-   int elsePending;
-   int elseStart;
-   /* Pending variable-registers */
-   VarStack* vpend;
-   /* Pending temp-registers */
-   IntSet* tpend;
-   /* Line number of detected constructs. */
-   LoopItem* loop_tree;
-   LoopItem* loop_ptr;
-   List breaks;
-   List continues;
-   //IntSet* repeats;
-   //IntSet* untils;
-   /* Control of do/end blocks. */
-   IntSet* do_opens;
-   IntSet* do_closes;
-   int released_local;
-   /* Skip for-variables on do-end check */
-   int ignore_for_variables;
-   int freeLocal;
-   /* boolean operations */
-   BoolOp* bools[MAXARG_A];
-   int nextBool;
-   Endif* nextEndif;
+	/* program counter during symbolic interpretation */
+	int pc;
+	/* These act as the VM registers */
+	char *R[MAXARG_A];
+	/* These store the priority for the operation that stored the value in each
+	  register */
+	int Rprio[MAXARG_A];
+	/* Boolean values indicating if register holds a table */
+	int Rtabl[MAXARG_A];
+	/* Registers standing for local variables. */
+	int Rvar[MAXARG_A];
+	/* Registers used in call returns. */
+	int Rcall[MAXARG_A];
+	/* This is the Lua proto for the function */
+	const Proto *f;
+	/* This is a stack for creation of tables */
+	List tables;
+	/* 'a' of last CALL instruction -- used with 0-param CALLs */
+	int lastCall;
+	/* Pending code to be flushed */
+	/* FIXME: Needs a better data structure */
+	char* vpendVal[MAXARG_A];
+	int internal[MAXARG_A];
+	int Rpend[MAXARG_A];
+	/* State variables for the TEST instruction. */
+	int testpending;
+	int elsePending;
+	int elseStart;
+	/* Pending variable-registers */
+	List vpend;
+	/* Pending temp-registers */
+	IntSet* tpend;
+	/* Line number of detected constructs. */
+	LoopItem* loop_tree;
+	LoopItem* loop_ptr;
+	List breaks;
+	List continues;
+	//IntSet* repeats;
+	//IntSet* untils;
+	/* Control of do/end blocks. */
+	IntSet* do_opens;
+	IntSet* do_closes;
+	int released_local;
+	/* Skip for-variables on do-end check */
+	int ignore_for_variables;
+	int freeLocal;
+	/* boolean operations */
+	BoolOp* bools[MAXARG_A];
+	int nextBool;
+	Endif* nextEndif;
 
-   List statements;
-   int firstLine;
-   int lastLine;
-   /* holds the printed function */
-   StringBuffer* decompiledCode;
-   /* indent */
-   int indent;
-   int elseWritten;
-   int testjump;
+	List statements;
+	int firstLine;
+	int lastLine;
+	/* holds the printed function */
+	StringBuffer* decompiledCode;
+	/* indent */
+	int indent;
+	int elseWritten;
+	int testjump;
 
-	 /* internal variable stack */
-	 int intspos;
-	 int intbegin[100];
-	 int intend[100];
+	/* internal variable stack */
+	int intspos;
+	int intbegin[100];
+	int intend[100];
 };
 
 typedef struct DecTableItem_ DecTableItem;
 
 struct DecTableItem_ {
-   ListItem super;
-   char *value;
-   int numeric;
-   char *key;
+	ListItem super;
+	char *value;
+	int numeric;
+	char *key;
 };
 
 typedef struct DecTable_ DecTable;
 
 struct DecTable_ {
-   ListItem super;
-   int reg;
-   int topNumeric;
-   List numeric;
-   List keyed;
-   int keyedSize;
-   int arraySize;
-   int used;
-   int pc;
-   Function *F;
+	ListItem super;
+	int reg;
+	int topNumeric;
+	List numeric;
+	List keyed;
+	int keyedSize;
+	int arraySize;
+	int used;
+	int pc;
+	Function *F;
 };
 
 typedef struct Variable_ Variable;
 
 struct Variable_ {
-   ListItem super;
-   char *name;
-   int reg;
+	ListItem super;
+	char *name;
+	int reg;
 };
 
 typedef struct IntListItem_ IntListItem;
@@ -173,16 +173,16 @@ const char *GetR(Function * F, int r);
 typedef struct LogicExp_ LogicExp;
 
 struct LogicExp_ {
-   LogicExp* parent;
-   LogicExp* next;
-   LogicExp* prev;
-   LogicExp* subexp;
-   int is_chain;
-   char* op1;
-   char* op2;
-   OpCode op;
-   int dest;
-   int neg;
+	LogicExp* parent;
+	LogicExp* next;
+	LogicExp* prev;
+	LogicExp* subexp;
+	int is_chain;
+	char* op1;
+	char* op2;
+	OpCode op;
+	int dest;
+	int neg;
 };
 
 LogicExp* MakeExpNode(BoolOp* boolOp);
