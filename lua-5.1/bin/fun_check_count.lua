b0c0a7a2df0621209524a31d4bbc36cc27c6c814
viruscamp <viruscamp@gmail.com>
2014-10-03 21:11:14 +0800

    close #25 Script to to count the result of function check

---
+++ lua-5.1/bin/fun_check_count.lua
@@ -0,0 +1,22 @@
+local string_find = string.find
+
+local files, passed, failed, uncompilable = 0, 0, 0, 0
+
+local filename = ...
+local f = filename and assert(io.open(filename, "r")) or io.stdin
+for line in f:lines() do
+	if string_find(line, '^%s*-- function check pass') then
+		passed = passed + 1
+	elseif string_find(line, '^%s*-- function check fail') then
+		failed = failed + 1
+		if string_find(line, '^%s*-- function check fail [%d_]+ : cannot compile') then
+			uncompilable = uncompilable + 1
+		end
+	elseif string_find('^%s*-- function check Command line') then
+		files = files + 1
+	end
+end
+f:close()
+
+local fmt = "files: %d, functions: %d, passed: %d, failed: %d, uncompilable: %d, pass rate: %.2f%%"
+print(fmt:format(files, passed + failed, passed, failed, uncompilable, 100*passed/(passed + failed)))
