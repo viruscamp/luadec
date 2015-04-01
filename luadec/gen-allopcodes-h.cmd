7d032e60633882933d7fdc5f6194284e2fcf727b
viruscamp <viruscamp@gmail.com>
2015-04-01 18:47:54 +0800

    fix build script for luaopswap to use correct allopcodes-*.lua

--- luadec/gen-allopcodes-h.cmd
+++ luadec/gen-allopcodes-h.cmd
@@ -1,11 +1,12 @@
 @echo off
-set BIN_PATH=%1
-echo copying allopcodes.lua
-copy /y ..\bin\allopcodes.lua allopcodes.lua
+set OUTDIR=%1
+set LUAFILE=%2
+echo copying %LUAFILE%
+copy /y ..\bin\%LUAFILE% allopcodes.lua
 echo generating allopcodes.luac
-%BIN_PATH%\luac -o allopcodes.luac allopcodes.lua
+%OUTDIR%\luac -o allopcodes.luac allopcodes.lua
 echo generating allopcodes_lua.h
-%BIN_PATH%\lua ..\bin\bin2c.lua allopcodes.lua allopcodes_lua > allopcodes_lua.h
+%OUTDIR%\lua ..\bin\bin2c.lua allopcodes.lua allopcodes_lua > allopcodes_lua.h
 echo generating allopcodes_luac.h
-%BIN_PATH%\lua ..\bin\bin2c.lua allopcodes.luac allopcodes_luac > allopcodes_luac.h
+%OUTDIR%\lua ..\bin\bin2c.lua allopcodes.luac allopcodes_luac > allopcodes_luac.h
 @echo on
