a8e484596e2cf6701b819db5830a98e009f36659
viruscamp <viruscamp@gmail.com>
2015-04-26 12:14:17 +0800

    * move all compiled test files to test-compiled

--- bin/test-long.bat
+++ bin/test-long.bat
@@ -1,47 +1,41 @@
 @echo luadec long test
 
-del error_fc.txt error_fc_ns.txt error_fc_fn.txt error_fc_s.txt
-del result_fc.txt result_fc_ns.txt result_fc_fn.txt result_fc_s.txt
+del test_fc_error.txt test_fc_ns_error.txt test_fc_fn_error.txt test_fc_s_error.txt
+del test_fc_error_c.txt test_fc_ns_error_c.txt test_fc_fn_error_c.txt test_fc_s_error_c.txt
+del test_fc_summary.txt test_fc_ns_summary.txt test_fc_fn_summary.txt test_fc_s_summary.txt
+del test_fc_summary_c.txt test_fc_ns_summary_c.txt test_fc_fn_summary_c.txt test_fc_s_summary_c.txt
 
-for /r ..\test %%f in (*.lua) do @luadec -fc %%f 2>>error_fc.txt | findstr /r /c:"-- function check" >>result_fc.txt
-for /r ..\lua-5.1\test %%f in (*.lua) do @luadec -fc %%f 2>>error_fc.txt | findstr /r /c:"-- function check" >>result_fc.txt
+for /r ..\test %%f in (*.lua) do @luadec -fc %%f 2>>test_fc_error.txt | findstr /r /c:"-- function check" >>test_fc_summary.txt
+for /r ..\test-compiled %%f in (*.lua) do @luadec -fc %%f 2>>test_fc_error_c.txt | findstr /r /c:"-- function check" >>test_fc_summary_c.txt
 
-for /r ..\test %%f in (*.lua) do @luadec -fc -ns %%f 2>>error_fc_ns.txt | findstr /r /c:"-- function check" >>result_fc_ns.txt
-for /r ..\lua-5.1\test %%f in (*.lua) do @luadec -fc -ns %%f 2>>error_fc_ns.txt | findstr /r /c:"-- function check" >>result_fc_ns.txt
+for /r ..\test %%f in (*.lua) do @luadec -fc -ns %%f 2>>test_fc_ns_error.txt | findstr /r /c:"-- function check" >>test_fc_ns_summary.txt
+for /r ..\test-compiled %%f in (*.lua) do @luadec -fc -ns %%f 2>>test_fc_ns_error_c.txt | findstr /r /c:"-- function check" >>test_fc_ns_summary_c.txt
 
-for /r ..\test %%f in (*.lua) do @luadec -fc -fn 0_3 %%f 2>>error_fc_fn.txt | findstr /r /c:"-- function check" >>result_fc_fn.txt
-for /r ..\lua-5.1\test %%f in (*.lua) do @luadec -fc -fn 0_3 %%f 2>>error_fc_fn.txt | findstr /r /c:"-- function check" >>result_fc_fn.txt
+for /r ..\test %%f in (*.lua) do @luadec -fc -fn 0_3 %%f 2>>test_fc_fn_error.txt | findstr /r /c:"-- function check" >>test_fc_fn_summary.txt
+for /r ..\test-compiled %%f in (*.lua) do @luadec -fc -fn 0_3 %%f 2>>test_fc_fn_error_c.txt | findstr /r /c:"-- function check" >>test_fc_fn_summary_c.txt
 
-rem for /r ..\test %%f in (*.lua) do @luadec -s -fc %%f 2>>error_fc_s.txt | findstr /r /c:"-- function check" >>result_fc_s.txt
-rem for /r ..\lua-5.1\test %%f in (*.lua) do @luadec -s -fc %%f 2>>error_fc_s.txt | findstr /r /c:"-- function check" >>result_fc_s.txt
-
-
-del error_dis.txt
-
-@(echo luadec -dis)>>error_dis.txt
-for /r ..\test %%f in (*.lua) do @luadec -dis %%f 2>>error_dis.txt 1>nul
-for /r ..\lua-5.1\test %%f in (*.lua) do @luadec -dis %%f 2>>error_dis.txt 1>nul
-
-@(echo.)>>error_dis.txt
-@(echo luadec -dis -ns)>>error_dis.txt
-for /r ..\test %%f in (*.lua) do @luadec -dis -ns %%f 2>>error_dis.txt 1>nul
-for /r ..\lua-5.1\test %%f in (*.lua) do @luadec -dis -ns %%f 2>>error_dis.txt 1>nul
-
-@(echo.)>>error_dis.txt
-@(echo luadec -dis -fn 0_3)>>error_dis.txt
-for /r ..\test %%f in (*.lua) do @luadec -dis -fn 0_3 %%f 2>>error_dis.txt 1>nul
-for /r ..\lua-5.1\test %%f in (*.lua) do @luadec -dis -fn 0_3 %%f 2>>error_dis.txt 1>nul
+@rem for /r ..\test %%f in (*.lua) do @luadec -fc -s %%f 2>>test_fc_s_error.txt | findstr /r /c:"-- function check" >>test_fc_s_summary.txt
+@rem for /r ..\test-compiled %%f in (*.lua) do @luadec -fc -s %%f 2>>test_fc_s_error_c.txt | findstr /r /c:"-- function check" >>test_fc_s_summary_c.txt
 
 
-del error_fc_c.txt result_fc_c.txt
-for /r ..\test\compiled %%f in (*.lua) do @luadec -fc %%f 2>>error_fc_c.txt | findstr /r /c:"-- function check" >>result_fc_c.txt
+del test_dis_error.txt
+
+@(echo luadec -dis)>>test_dis_error.txt
+for /r ..\test %%f in (*.lua) do @luadec -dis %%f 2>>test_dis_error.txt 1>nul
+for /r ..\test-compiled %%f in (*.lua) do @luadec -dis %%f 2>>test_dis_error.txt 1>nul
+
+@(echo.)>>test_dis_error.txt
+@(echo luadec -dis -ns)>>test_dis_error.txt
+for /r ..\test %%f in (*.lua) do @luadec -dis -ns %%f 2>>test_dis_error.txt 1>nul
+for /r ..\test-compiled %%f in (*.lua) do @luadec -dis -ns %%f 2>>test_dis_error.txt 1>nul
+
+@(echo.)>>test_dis_error.txt
+@(echo luadec -dis -fn 0_3)>>test_dis_error.txt
+for /r ..\test %%f in (*.lua) do @luadec -dis -fn 0_3 %%f 2>>test_dis_error.txt 1>nul
+for /r ..\test-compiled %%f in (*.lua) do @luadec -dis -fn 0_3 %%f 2>>test_dis_error.txt 1>nul
 
 
-rem del error_fc_ab.txt result_fc_ab.txt
-rem for /r ..\angrybirds_lua %%f in (*.lua) do @luadec -fc %%f 2>>error_fc_ab.txt | findstr /r /c:"-- function check" >>result_fc_ab.txt
-rem @lua fun_check_count.lua result_fc_ab.txt
-
-
-lua fun_check_count.lua result_fc.txt
-lua fun_check_count.lua result_fc_c.txt
-lua fun_check_count.lua result_fc_s.txt
+lua fun_check_count.lua test_fc_summary.txt
+lua fun_check_count.lua test_fc_summary_c.txt
+@rem lua fun_check_count.lua test_fc_s_summary.txt
+@rem lua fun_check_count.lua test_fc_s_summary_c.txt
